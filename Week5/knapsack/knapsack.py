541182dadc250f56982d8d565335dcd8976940d5
AmanLP7 <ak547276@gmail.com>
2017-10-20 21:50:46 +0530

    corrected indexing

--- Week5/knapsack/knapsack.py
+++ Week5/knapsack/knapsack.py
@@ -23,16 +23,23 @@ def optimal_weight(W, w):
     value = [[0 for x in range(W+1)]
                        for y in range(len(w) + 1)]
 
+
+
     for i in range(1, len(w) + 1):
     	for weights in range(1, W+1):
     		value[i][weights] = value[i-1][weights]
-    		if w[i] < weights:
-    			val = value[i-1][weights-w[i]] + w[i]
+    		if w[i-1] < weights:
+    			val = value[i-1][weights-w[i-1]] + w[i-1]
     			if value[i][weights] < val:
     				value[i][weights] = val
 
 
-    return(value[n][W])
+    for rows in value:
+    	print(*rows,end = " ")
+    	print('\n')
+
+
+    return(value[len(w)][W])
  
  
 
@@ -41,7 +48,7 @@ def optimal_weight(W, w):
 
 if __name__ == '__main__':
     W, n, *w = list(map(int, input().split(" ")))
-    print(W,n,w)
+    print(optimal_weight(W,w))
 
 
 ##=========================================================##

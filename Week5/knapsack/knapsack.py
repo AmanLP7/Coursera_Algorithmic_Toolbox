342be2556ce6f4ef1b876259de78a07a0301678a
AmanLP7 <ak547276@gmail.com>
2017-10-20 23:39:38 +0530

    corrected if condition

--- Week5/knapsack/knapsack.py
+++ Week5/knapsack/knapsack.py
@@ -4,16 +4,23 @@ import random
 
 ##=========================================================##
 
-         ########################################
-         ##### Knapscak without repititions #####
-         #####    Time complexity => O(n)   #####
-         ########################################
+         #########################################
+         ##### Knapsack without repititions ######
+         #####    Time complexity => O(nW)   #####
+         #########################################
 
 
 ## Step 1 -> Take a two dimensional array, having length W, 
 ##           and breadth n.
 
-## Step 2 -> 
+## Step 2 -> Fill the "value" matrix using following logic,
+##           
+##           Case 1 -> nth item is included in solution so,
+##                     the subproblem is knapsack of weight
+##                     W - w(n), using items 1,2,...,n-1
+##           Case 2 -> nth item is not included then whole
+##                     knapsack must be filled with items
+##                     1,2,....n-1.
 
 
 
@@ -28,16 +35,16 @@ def optimal_weight(W, w):
     for i in range(1, len(w) + 1):
     	for weights in range(1, W+1):
     		value[i][weights] = value[i-1][weights]
-    		if w[i-1] < weights:
+    		if w[i-1] <= weights:
     			val = value[i-1][weights-w[i-1]] + w[i-1]
     			if value[i][weights] < val:
     				value[i][weights] = val
 
-
+    '''
     for rows in value:
     	print(*rows,end = " ")
     	print('\n')
-
+    '''
 
     return(value[len(w)][W])
  
@@ -45,6 +52,15 @@ def optimal_weight(W, w):
 
 ##=========================================================##
 
+             ####################################
+             ###  Knapsack without repitions  ###
+             ###      recursive solution      ###
+             ###   Time complexity => O()     ###
+             ####################################
+
+
+
+##=========================================================##
 
 if __name__ == '__main__':
     W, n, *w = list(map(int, input().split(" ")))

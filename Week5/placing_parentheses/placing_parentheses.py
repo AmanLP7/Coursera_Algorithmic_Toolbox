818f951fa5d0e38e48f24e8861d2f073c9a4887b
AmanLP7 <ak547276@gmail.com>
2017-11-02 21:52:59 +0530

    added comments

--- Week5/placing_parentheses/placing_parentheses.py
+++ Week5/placing_parentheses/placing_parentheses.py
@@ -1,4 +1,24 @@
 # Uses python3
+
+
+##-------------------------------------------------------------------##
+
+
+                 #####################################
+                 ######   Placing parantheses   ######
+                 ######  Time Complexity =>     ######
+                 #####################################
+
+
+## Input -> String S, of length 2n+1 for some n, with symbols s(0),
+##          s(1),s(2),.....,s(2n). Symbol at even position of S is a
+##          digit, symbol at odd position is one of three operations
+##          {+,-,*}.
+## Output -> Maximum possible value of arithmetic expression.
+
+
+
+## Function to evaluate expression                 
 def evalt(a, b, op):
     if op == '+':
         return a + b
@@ -9,9 +29,16 @@ def evalt(a, b, op):
     else:
         assert False
 
+
+
+## Function to get maximum value using dynamic programming.
 def get_maximum_value(dataset):
     #write your code here
     return 0
+
+
+
+##-------------------------------------------------------------------##
 
 
 if __name__ == "__main__":

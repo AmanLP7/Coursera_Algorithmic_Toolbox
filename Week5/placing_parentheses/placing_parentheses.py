d3ba3c95673f33c99aad51e3cc136107e98b5f9e
AmanLP7 <ak547276@gmail.com>
2017-11-04 18:21:24 +0530

    completed minmax function

--- Week5/placing_parentheses/placing_parentheses.py
+++ Week5/placing_parentheses/placing_parentheses.py
@@ -17,6 +17,17 @@
 ## Output -> Maximum possible value of arithmetic expression.
 
 
+## Function used ==>> 
+## 
+## 1. evalt() - It performs the given operation on an expression.
+## 
+## 2. MinAndMax() - It calculates the maximum and minimum value for
+##                  a sub-expression.
+##
+## 3. get_maximum_value() - It calculates the maximum possible value
+##                          for an expression by reordering, 
+##                          parantheses.  
+
 
 
 
@@ -33,20 +44,35 @@ def evalt(a, b, op):
 
 
 
-## Function to calculate minimum and maximum value for an expression
+
+
+
+## Function to calculate minimum and maximum value for an expression.
+## The function returns a list whose first value is the minimum value,
+## and the second value is a maximum value.
 def MinAndMax(i,j):
-    min = -float("Inf")
-    max = float("Inf")
+    minVal = -float("Inf")
+    maxVal = float("Inf")
 
     #Loop to calculate minimum and maximum value
     for k in range(i,j):
-        a = M[i][k]
+        a = evalt(M[i][k], M[k+1][j], operator[k])
+        b = evalt(M[i][k], m[k+1][j], operator[k])
+        c = evalt(m[i][k], M[k+1][j], operator[k])
+        d = evalt(m[i][k], m[k+1][j], operator[k])
+
+        minMax = [min(minVal, a, b, c, d), max(maxVal, a, b, c, d)]
+
+
+    return(minMax)
+
+
+
 
 
 ## Function to get maximum value using dynamic programming.
 def get_maximum_value(dataset):
-    #write your code here
-    return 0
+
 
 
 
@@ -54,4 +80,11 @@ def get_maximum_value(dataset):
 
 
 if __name__ == "__main__":
-    print(get_maximum_value(input()))
+    expression = list(input())
+    digit = expression[0:len(expression)+1:2]
+    operator = expression[1:len(expression)+1:2]
+    print(digit,operator)
+
+
+
+##-------------------------------------------------------------------##
